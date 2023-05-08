484761e94dee4295af1dd72d27d7ccb169c28d09
avizyt <avijit.biswas27@outlook.com>
2023-05-09 01:59:38 +0530

    changes some configs and folder names

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+    logo: <span>Learning Academy</span>,
+    project: {
+        link: "https://github.com/avizyt/avijit-blog",
+    },
+    chat: {
+        link: "https://discord.com",
+    },
+    docsRepositoryBase: "https://github.com/avizyt/avijit-blog",
+    footer: {
+        text: "Learning Academy",
+    },
+};
 
-export default config
+export default config;
